f58885786f2d0f17fe6978d33da4a347380fba8f
choisungwook <kgg1959@naver.com>
2024-05-04 16:02:20 +0900

    KAN-25 Fix python syntax (#5)

--- manifests/example-2-circuitbreak/python_locust/main.py
+++ manifests/example-2-circuitbreak/python_locust/main.py
@@ -5,7 +5,7 @@ class UserBehaviour(TaskSet):
   @task
   def sleep_endpoint(self):
       response = self.client.get("/sleep")
-      print("status code: {response.status_code}")
+      print(f"status code: {response.status_code}")
 
 class MyUser(HttpUser):
   wait_time = between(1, 5)
